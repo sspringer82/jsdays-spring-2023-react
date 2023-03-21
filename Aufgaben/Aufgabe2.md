0ad984c500454805d3d7f0c546d3e73d9d0a8179
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2023-03-21 14:43:08 +0100

    Aufgabe 2

---
+++ Aufgaben/Aufgabe2.md
@@ -0,0 +1,5 @@
+# 02 - React Router
+
+`npm install react-router-dom`
+
+Trenne die Implementierung von Form und List. Erzeuge zwei neue Routen: /create und /edit/:id, um das Formular zu öffnen.
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import List from './List';
 import { PersonProvider } from './PersonProvider';
+import { BrowserRouter, Route, Routes, useNavigate, useParams } from 'react-router-dom';
 
 const App: React.FC = () => {
+  const navigate = useNavigate();
+  // navigate('/new');
+  // navigate('/edit/42')
+  const { id } = useParams<{ id: string }>();
   return (
-    <PersonProvider>
-      <List />
-    </PersonProvider>
+    <BrowserRouter>
+      <PersonProvider>
+        <Routes>
+          <Route path="/" element={<List />} />
+          <Route path="/edit/:id" element={<List />} />
+        </Routes>
+      </PersonProvider>
+    </BrowserRouter>
   );
 };
 
